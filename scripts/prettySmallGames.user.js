a5a3e07efdf9203d934947c147b40381f9878d82
Vladislav Maksimov <46715602+VladislavMaksimov@users.noreply.github.com>
2024-06-29 00:57:26 +0200

    UPDATE: Pretty Small Games - responsive text

--- scripts/prettySmallGames.user.js
+++ scripts/prettySmallGames.user.js
@@ -13,17 +13,22 @@
     'use strict';
 
     GM_addStyle(`
-    p, a, span, b, strong, i, th, td, div, input[type=button], input[type=text], input[type=text]::placeholder, textarea, .chatbro_send_input::before {
-        font-size: 14px !important;
-        line-height: 150% !important;
-    }
-
-    h3 {
-        font-size: 16px !important;
-        line-height: 150% !important;
-    }
-
-    .chatbro_send .chatbro_send_input_block .chatbro_send_input_wrapper .chatbro_send_input {
-        transform: scale(1) !important;
-    }`);
+        :root {
+            font-size: 62.5%;
+        }
+    
+        p, a, span, b, strong, i, th, td, div, input[type=button], input[type=text], input[type=text]::placeholder, textarea, .chatbro_send_input::before {
+            font-size: 1.4rem !important;
+            line-height: 150% !important;
+        }
+    
+        h3 {
+            font-size: 1.6rem !important;
+            line-height: 150% !important;
+        }
+    
+        .chatbro_send .chatbro_send_input_block .chatbro_send_input_wrapper .chatbro_send_input {
+            transform: scale(1) !important;
+        }
+    `);
 })();
