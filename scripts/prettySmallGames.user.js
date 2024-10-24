c476cc4db6f4acd00d71b73070aa360baf1fd5a9
Vladislav Maksimov <workmaximov@yandex.ru>
2024-10-24 17:47:01 +0200

    UPDATE: PSG - garbage remover

--- scripts/prettySmallGames.user.js
+++ scripts/prettySmallGames.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pretty Small Games
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.0.2
 // @description  Improve Small Games old-school UI.
 // @author       Vladislav Maksimov
 // @match        https://small-games.info/*
@@ -9,26 +9,42 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  "use strict";
 
-    GM_addStyle(`
-        :root {
-            font-size: 62.5%;
-        }
-    
-        p, a, span, b, strong, i, th, td, div, input[type=button], input[type=text], input[type=text]::placeholder, textarea, .chatbro_send_input::before {
-            font-size: 1.4rem !important;
-            line-height: 150% !important;
-        }
-    
-        h3 {
-            font-size: 1.6rem !important;
-            line-height: 150% !important;
-        }
-    
-        .chatbro_send .chatbro_send_input_block .chatbro_send_input_wrapper .chatbro_send_input {
-            transform: scale(1) !important;
-        }
-    `);
+  GM_addStyle(`
+          :root {
+              font-size: 62.5%;
+          }
+      
+          p, a, span, b, strong, i, th, td, div, input[type=button], input[type=text], input[type=text]::placeholder, textarea, .chatbro_send_input::before {
+              font-size: 1.4rem !important;
+              line-height: 150% !important;
+          }
+      
+          h3 {
+              font-size: 1.6rem !important;
+              line-height: 150% !important;
+          }
+      
+          .chatbro_send .chatbro_send_input_block .chatbro_send_input_wrapper .chatbro_send_input {
+              transform: scale(1) !important;
+          }
+      `);
+
+  // remove all annoying shit such as ads and so on
+  const capitalistShitRemover = new MutationObserver(() => {
+    // remove chat's need-to-pay message
+    const chatBroShit = document.querySelector(
+      ".chatbro_messages_history_plug_td"
+    );
+    if (chatBroShit) {
+      chatBroShit.remove();
+    }
+  });
+
+  capitalistShitRemover.observe(document.body, {
+    subtree: true,
+    childList: true,
+  });
 })();
